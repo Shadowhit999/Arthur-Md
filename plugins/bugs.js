ffbbaa8002f87f6aa49f6051abc1fdd109a6b70e
STAR-KING0 <kingdavid23565@gmail.com>
2024-07-21 08:56:08 +0100

    Update bugs.js

--- plugins/bugs.js
+++ plugins/bugs.js
@@ -1,6 +1,4 @@
 
-//bug database
-const { xeontext1 } = require('./lib/bugs/xeontext1')
 const { xeontext2 } = require('./lib/bugs/xeontext2')
 const { xeontext3 } = require('./lib/bugs/xeontext3')
 const { xeontext4 } = require('./lib/bugs/xeontext4')
